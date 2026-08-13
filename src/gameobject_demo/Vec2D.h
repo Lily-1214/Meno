0f7341293773d685a3353c50c3e199641212fdb4
Choi SangJun <68640265+ajbuwidnnfjwj@users.noreply.github.com>
2026-08-13 22:10:51 +0900

    gameobject 템플릿

---
+++ src/gameobject_demo/Vec2D.h
@@ -0,0 +1,12 @@
+//
+// Created by 최상준 on 26. 8. 7..
+//
+
+#ifndef GAMEOBJCECT_DEMO_VEC2D_H
+#define GAMEOBJCECT_DEMO_VEC2D_H
+
+struct Vec2D {
+    int x, y;
+};
+
+#endif // GAMEOBJCECT_DEMO_VEC2D_H
